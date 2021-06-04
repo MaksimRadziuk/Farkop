83daf98a637aaacb1bd7fe3a35043abc4b009417
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2021-06-04 16:10:49 +0300

    04.06 16.10

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	  }
 	  var scrollDiv=$(this);
 	  $(window).scroll(function(){
-	    if($(window).scrollTop()=="0"){
+	    if($(window).scrollTop()<="500"){
 	    $(scrollDiv).fadeOut("slow")
 	    }else{
 	    $(scrollDiv).fadeIn("slow")
@@ -40,5 +40,76 @@ $(document).ready(function(){
 	$('.cookieBar .close').click(function(){
 		$('.cookieBar').hide();
 	})
+
+	$('header .navbar ul .close').click(function(){
+		$('header .navbar ul').toggleClass('active');
+	})
+	$('.hamburger').click(function(){
+		$('header .navbar ul').toggleClass('active');
+	})
+
+	$('.search__popup .close').click(function(){
+		$('.search__popup').toggleClass('active');
+	})
+
+	$('.phone__trigger').click(function(){
+		$('.info__wrap.phone_wrap').toggleClass('active');
+	})
+	$('.address__trigger').click(function(){
+		$('.info__wrap.address_wrap').toggleClass('active');
+	})
+	$('.info__wrap .close').click(function(){
+		$(this).parent('.info__wrap').toggleClass('active');
+	})
+
+	$('.filter .bottom_part').click(function(){
+		$('.filter_wrap ul').toggleClass('active');
+		$(this).toggleClass('active');
+	})
+
+
+	$('.service__slider').slick({
+	  dots:false,
+	  arrows: true,
+	  slidesToShow: 6,
+	  responsive: [
+	    {
+	      breakpoint: 1441,
+	      settings: {
+	        slidesToShow: 5
+	      }
+	    },
+	    {
+	      breakpoint: 1281,
+	      settings: {
+	        slidesToShow: 4
+	      }
+	    },
+	    {
+	      breakpoint: 910,
+	      settings: {
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint:700,
+	      settings: {
+	        slidesToShow: 2,
+	        arrows:false
+	      }
+	    }
+	  ]
+	});
+
+	$('.popup .close').click(function(){
+		$(this).parent('.popup').fadeOut("fast");
+		$('#background').fadeOut("slow");
+	})
+	$('.order__triger').click(function(){
+		$('.popup__order').fadeIn("slow");
+		$('#background').fadeIn("fast");
+	})
+
+	
 
 });
